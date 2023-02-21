471c3e423534586a7c8e3eaba9bb9cc3c21c7d4e
Po-Chun Huang <casper@Po-Chuns-MacBook-Pro.local>
2023-02-21 11:29:17 +0800

    first version completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,34 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    `gatsby-plugin-sharp`,
+    'gatsby-plugin-image',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `projects`,
+        // Path to the directory
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2023 Web Warrior',
+    contact: 'popo19921107@hotmail.com'
+  }
 }
